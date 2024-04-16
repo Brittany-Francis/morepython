0a321c2febb3f401bfd959637812d0a6e1f94f49
Brittany Francis <brittanyfrancis200@gmail.com>
2024-04-16 19:57:14 -0400

    loops learning

--- loops.py
+++ loops.py
@@ -43,3 +43,46 @@ for student in students_period_A:
   print(student)
 
 print(students_period_B)
+
+#break
+dog_breeds_available_for_adoption = ["french_bulldog", "dalmatian", "shihtzu", "poodle", "collie"]
+dog_breed_I_want = "dalmatian"
+
+for dog_breed in dog_breeds_available_for_adoption:
+  print(dog_breed)
+  if dog_breed == dog_breed_I_want:
+    print("They have the dog I want!")
+    break
+  
+#continue
+ages = [12, 38, 34, 26, 21, 19, 67, 41, 17]
+
+for age in ages:
+  if age<21:
+    continue
+  else:
+    print(age)
+
+#nested loops
+sales_data = [[12, 17, 22], [2, 10, 3], [5, 12, 13]]
+
+scoops_sold=0
+
+for location in sales_data:
+  for num in location:
+    scoops_sold+=num
+
+print(scoops_sold)
+#list comprehension
+# Your code below:
+single_digits=range(0, 10)
+for num in single_digits:
+  print(num)
+
+squares=[]
+for num in single_digits:
+  squares.append(num**2)
+
+print(squares)
+cubes=[num**3 for num in single_digits]
+print(cubes)
