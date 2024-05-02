74e41ff0a397b5de239c9729bfe441d3c9f68459
Brittany Francis <brittanyfrancis200@gmail.com>
2024-05-02 13:50:22 -0400

    python loop practice

--- python_loops_and_functions.py
+++ python_loops_and_functions.py
@@ -48,3 +48,54 @@ def exponents(bases, powers):
 
   return new_list
 print(exponents([2, 3, 4], [1, 2, 3]))
+
+#larger sum
+def larger_sum(lst1, lst2):
+  sum1=0
+  sum2=0
+  for num in lst1:
+    sum1+=num
+  for num in lst2:
+    sum2+=num
+  if sum1>=sum2:
+    return lst1
+  else:
+    return lst2
+
+
+print(larger_sum([1, 9, 5], [2, 3, 7]))
+
+#larger than 9000
+def over_nine_thousand(lst):
+  sum=0
+  if len(lst)==0:
+    return 0
+  for num in lst:
+    sum+=num
+    if sum>9000:
+      break
+  return sum
+
+print(over_nine_thousand([8000, 900, 120, 5000]))
+
+#max num
+def max_num(nums):
+  largest=nums[0]
+  for num in nums:
+    if num>largest:
+      largest=num
+  return largest
+
+print(max_num([50, -10, 0, 75, 20]))
+
+#same_value
+def same_values(lst1, lst2):
+  same_value=[]
+  for idx in range(len(lst1)):
+    if lst1[idx]==lst2[idx]:
+      same_value.append(idx)
+
+  return same_value
+    
+
+print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
