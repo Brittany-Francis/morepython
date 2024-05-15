33276b727c85a90e56f3a4c6ca8ad845c2703e3d
Brittany Francis <brittanyfrancis200@gmail.com>
2024-05-15 13:40:24 -0400

    string practice

--- strings.py
+++ strings.py
@@ -8,3 +8,21 @@ def username_generator(first_name, last_name):
   else:
     user_name= first_name+last_name
   return user_name
+
+def password_generator(user_name):
+  password=""
+  for idx in range(0, len(user_name)):
+    password+=user_name[idx-1]
+    print(password)
+  return password
+
+
+password_generator("Brittany")
+
+# def password_generator(user_name):
+#   password=""
+#   last=user_name[-1]
+#   first=user_name[:-1]
+#   print(last)
+#   print(first)
+#   return last+first
