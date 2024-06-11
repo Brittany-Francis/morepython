410966ed8847083919e6462d866a0459497325df
Brittany Francis <brittanyfrancis200@gmail.com>
2024-06-11 11:23:03 -0400

    dictionaries notes

---
+++ dictionaries.py
@@ -0,0 +1,26 @@
+#what dictionaries look like
+
+sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
+num_cameras = {"backyard": 6,  "garage": 2, "driveway": 1}
+
+print(sensors)
+translations={
+"mountain": "orod", 
+"bread": "bass",
+"friend": "mellon",
+"horse": "roch"
+}
+
+children = {"von Trapp":["Johannes", "Rosmarie", "Eleonore"] ,"Corleone": ["Sonny", "Fredo", "Michael"] }
+
+#keys cannot be lists b/c they are mutable
+#to add a key value pair
+
+#dictionary[key] = value
+
+animals_in_zoo={}
+animals_in_zoo["zebras"]=8
+animals_in_zoo["monkeys"]=12
+animals_in_zoo["dinosaurs"]=0
+print(animals_in_zoo)
+
